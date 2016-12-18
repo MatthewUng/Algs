297b160da8dc9cb84defb7cb9db93b9beff3f9b2
Matthew Ung <matthew.ung814@gmail.com>
2016-12-17 19:27:34 -0800

    changed source to algdb

--- PullAlgs.py
+++ PullAlgs.py
@@ -1,12 +1,30 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-url = "http://cubewhiz.com/oll.php"
+cubewhiz = "http://cubewhiz.com/oll.php"
+algdb = "http://algdb.net/Set/OLL"
 
-file = urllib2.urlopen(url)
+"""
+file = urllib2.urlopen(cubewhiz)
 soup = BeautifulSoup(file, 'html.parser')
 
 for table in soup.find_all("table"):
     for row in table.find_all("tr"):
         if row.b:
             print row.find("b").string
+"""
+
+file = urllib2.urlopen(algdb)
+soup = BeautifulSoup(file, "html.parser")
+
+
+for row in soup.find("table").find_all("tr"):
+    data = row.find_all("td")
+
+    if len(data) < 3:
+        continue
+    if "OLL" in data[0].string:
+        print data[0].get_text()
+        print data[2].get_text()
+
+
