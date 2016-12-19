3095047895a1fd3d00a3332e1857c1e469fdee1a
Matthew Ung <matthew.ung814@gmail.com>
2016-12-19 00:50:31 -0800

    boxes of UI change color working on adding edge and corners

--- UI.py
+++ UI.py
@@ -2,34 +2,79 @@ from tkinter import *
 
 class CubeGrid(Canvas):
     squareSize = 150
+    edgeSize = 25
+    border = 50
     offset = 5
-    def __init__(self, master, w=600, h=600):
-        Canvas.__init__(self, master,width=w, height=h)
-        self.grid = [[True, True, True] for _ in range(3)]
+
+    def __init__(self, master):
+        # init values for convenience
+        side = CubeGrid.squareSize
+        edge = CubeGrid.edgeSize
+        bord = CubeGrid.border
+        offset = CubeGrid.offset
+
+        wh = 2* bord + 3* side
+
+        Canvas.__init__(self, master,width=wh, height=wh)
+
+
+        self.grid = [[0, 0, 0] for _ in range(3)]
         self.squares = [[0,0,0] for _ in range(3)]
+        self.edges = [[] for _ in range(4)]
+        # 0 1
+        # 2 3
+        # = i/2 + 2*j
+        self.corners = [[] for _ in range(4)]
         for i in range(3):
             for j in range(3):
-
-                self.squares[i][j] = self.create_rectangle(105+Canvas.squareSize*i, 105+Canvas.squareSize*j,
-                                                           245+Canvas.SquareSize*i, 245+Canvas.squareSize*j,
+                self.squares[i][j] = self.create_rectangle(bord+offset+side*i, bord+offset+side*j,
+                                                           bord-offset+side+side*i, bord-offset+side+side*j,
                                                            fill="yellow", tags=str(i)+str(j))
+
                 self.tag_bind(self.squares[i][j], '<ButtonPress-1>', self.onObjectClick)
 
         self.pack()
+
+    def is_edge(self, i,j):
+        if i == 1 and j == 1:
+            return False
+        elif self.is_corner(i,j):
+            return False
+        else: return True
+
+    def is_corner(self, i, j ):
+        if i!= 0 or i != 2 or j != 0 or j!= 2:
+            return False
+        else: return True
 
     def getValues(self):
         return self.grid
 
     def changeValue(self,i,j):
+        if self.itemcget(self.squares[i][j], "fill") == "yellow":
 
-        self.grid[i][j] = not self.grid[i][j]
-        print self.getValues()
+            self.itemconfig(self.squares[i][j], fill="black")
+        else:
+            self.itemconfig(self.squares[i][j], fill="yellow")
+
+        # edges have 2 orientations
+        if i != 0 or i != 2 or j != 0 or j != 2:
+            self.grid[i][j] = (self.grid[i][j] + 1) % 2
+        # corners have 3 orientations
+        else:
+            self.grid[i][j] = (self.grid[i][j] + 1) % 3
 
 
     def onObjectClick(self, event):
-        print event.x, event.y
-        print event.widget
-        print event.widget.find_closest(event.x,event.y)
+        #print event.x, event.y
+        #print event.widget
+        #print event.widget.find_closest(event.x,event.y)
+        for i in range(3):
+            for j in range(3):
+                if event.widget.find_closest(event.x, event.y)[0] == self.squares[i][j]:
+                    self.changeValue(i, j)
+
+                    return
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,41 +1,16 @@
-from bs4 import BeautifulSoup
-import urllib2
-import pandas as pd
+from tkinter import *
 
-wiki = "https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_in_India"
+def motion(event):
+    print "Mouse Position: (%s %s)" % (event.x, event.y)
+    return
 
-page = urllib2.urlopen(wiki)
+master = Tk()
+Whatever_you_do = """Whatever you do will be insignificant, but it is very important that you do
+it.\n(Mahatma Gandhi)"""
 
-soup = BeautifulSoup(page, "html.parser")
+msg = Message(master, text = Whatever_you_do)
+msg.config(bg="lightgreen", font=("times", 24, "italic"))
+msg.bind("<Motion>", motion)
+msg.pack()
 
-
-right_table = soup.find('table',class_='wikitable sortable plainrowheaders')
-
-A = []
-B = []
-C = []
-D = []
-E = []
-F = []
-G = []
-
-for row in right_table.findAll("tr"):
-    cells = row.findAll('td')
-    states = row.findAll('th') # second colomn data
-    if len(cells) == 6:
-        A.append(cells[0].find(text=True))
-        B.append(states[0].find(text=True))
-        C.append(cells[1].find(text=True))
-        D.append(cells[2].find(text=True))
-        E.append(cells[3].find(text=True))
-        F.append(cells[4].find(text=True))
-        G.append(cells[5].find(text=True))
-
-df = pd.DataFrame(A,columns=['Numbers'])
-df['State/UT'] = B
-df['Admin_Captital']=C
-df['Legislative_Capital']=D
-df['Judiciary_Capital']=E
-df['Year_Capital']=F
-df['Former_Capital']=G
-df
+mainloop()
